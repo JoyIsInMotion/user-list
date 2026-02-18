0897f1528aed9fa7cf417e29de597d5e6232b935
JoyIsInMotion <joyisinmotion@gmail.com>
2026-02-18 14:22:29 +0200

    added header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import react from 'react'
+
+const Header = () => {
+    return (
+        <>
+            {/* Header component */}
+            <header className="header">
+                <div className="logo">
+                    <span className="course">React Workshop - September 2023</span>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header;
+
+
