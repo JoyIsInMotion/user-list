2300c39f3862512750e20123d200abe14d701f59
JoyIsInMotion <joyisinmotion@gmail.com>
2026-02-19 12:44:46 +0200

    extracting main components

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <>
+            {/* Footer component  */}
+            <footer className="footer">
+                <p>This site is designed to be used for training purposes at SoftUni.</p>
+            </footer>
+
+        </>
+    )
+}
+
+export default Footer;
